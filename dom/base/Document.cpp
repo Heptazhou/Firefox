d3e15502fd30d26d2a26c3800d0ba928d4793eca
Heptazhou <zhou@0h7z.com>
2023-01-02 23:07:45 +0000

    Update - rfp/fpp
    Document::PreferredColorScheme
    DocumentAcceptHeader
    ImageAcceptHeader
    nsHttpHandler::Init
    nsHttpHandler::PrefsChanged
    nsRFPService::GetSpoofedUserAgent
    nsScreen::GetAvailRect
    RFPHelper
    RuntimeService::ClampedHardwareConcurrency
    SessionStoreInternal
    
    browser/components/sessionstore/SessionStore.sys.mjs
    dom/base/Document.cpp
    dom/base/nsScreen.cpp
    dom/workers/RuntimeService.cpp
    netwerk/protocol/http/nsHttpHandler.cpp
    toolkit/components/resistfingerprinting/nsRFPService.h
    toolkit/components/resistfingerprinting/RFPHelper.sys.mjs

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -18885,7 +18885,7 @@ ColorScheme Document::DefaultColorScheme() const {
 ColorScheme Document::PreferredColorScheme(IgnoreRFP aIgnoreRFP) const {
   if (ShouldResistFingerprinting(RFPTarget::CSSPrefersColorScheme) &&
       aIgnoreRFP == IgnoreRFP::No) {
-    return ColorScheme::Light;
+    return ColorScheme::Dark;
   }
 
   if (nsPresContext* pc = GetPresContext()) {
