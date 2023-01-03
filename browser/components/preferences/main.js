dad5207a20a5ebefb521ecff1bc64853efc4e621
Heptazhou <zhou@0h7z.com>
2023-01-03 21:45:52 +0000

    Update
    about:addons
    about:policies
    about:preferences
    about:serviceworkers
    
    browser/components/enterprisepolicies/content/aboutPolicies.css
    browser/components/enterprisepolicies/content/aboutPolicies.js
    browser/components/preferences/main.inc.xhtml
    browser/components/preferences/main.js
    browser/components/preferences/privacy.inc.xhtml
    browser/components/preferences/privacy.js
    browser/components/urlbar/QuickActionsLoaderDefault.sys.mjs
    browser/locales/en-US/browser/policies/policies-descriptions.ftl
    toolkit/locales/en-US/toolkit/branding/accounts.ftl
    toolkit/locales/en-US/toolkit/branding/brandings.ftl
    toolkit/themes/shared/aboutServiceWorkers.css

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -1715,7 +1715,7 @@ var gMainPane = {
       let alwaysCheckPref = Preferences.get(
         "browser.shell.checkDefaultBrowser"
       );
-      alwaysCheck.disabled = alwaysCheckPref.locked || isDefault;
+      alwaysCheck.disabled = alwaysCheckPref.locked;
     }
   },
 
