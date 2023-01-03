5038f3955993c0fcce488cbeae8ce85bc3a05562
Heptazhou <zhou@0h7z.com>
2023-01-03 21:45:52 +0000

    Update
    about:addons
    about:buildconfig
    about:policies
    about:preferences
    about:serviceworkers
    
    browser/components/enterprisepolicies/content/aboutPolicies.css
    browser/components/enterprisepolicies/content/aboutPolicies.js
    browser/components/preferences/main.inc.xhtml
    browser/components/preferences/main.js
    browser/components/preferences/preferences.js
    browser/components/preferences/preferences.xhtml
    browser/components/preferences/privacy.inc.xhtml
    browser/components/preferences/privacy.js
    browser/components/urlbar/QuickActionsLoaderDefault.sys.mjs
    toolkit/components/extensions/Extension.sys.mjs
    toolkit/content/buildconfig.html
    toolkit/themes/shared/aboutServiceWorkers.css

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -1737,7 +1737,7 @@ var gMainPane = {
       let alwaysCheckPref = Preferences.get(
         "browser.shell.checkDefaultBrowser"
       );
-      alwaysCheck.disabled = alwaysCheckPref.locked || isDefault;
+      alwaysCheck.disabled = alwaysCheckPref.locked;
     }
   },
 
