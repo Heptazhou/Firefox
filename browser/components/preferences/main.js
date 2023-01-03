a21da8a0568456489489c76e0160ab7b3b4b29ed
Heptazhou <zhou@0h7z.com>
2023-01-03 21:45:52 +0000

    Update
    about:addons
    about:policies
    about:preferences
    
    browser/components/enterprisepolicies/content/aboutPolicies.css
    browser/components/enterprisepolicies/content/aboutPolicies.js
    browser/components/preferences/main.inc.xhtml
    browser/components/preferences/main.js
    browser/components/preferences/privacy.inc.xhtml
    browser/components/preferences/privacy.js
    browser/components/urlbar/QuickActionsLoaderDefault.sys.mjs
    browser/locales/en-US/browser/branding/brandings.ftl
    browser/locales/en-US/browser/branding/sync-brand.ftl
    browser/locales/en-US/browser/policies/policies-descriptions.ftl

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -1727,7 +1727,7 @@ var gMainPane = {
       let alwaysCheckPref = Preferences.get(
         "browser.shell.checkDefaultBrowser"
       );
-      alwaysCheck.disabled = alwaysCheckPref.locked || isDefault;
+      alwaysCheck.disabled = alwaysCheckPref.locked;
     }
   },
 
