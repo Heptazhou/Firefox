6cdd0abb85c389678f28e9cb5a3c49ab0092480e
Heptazhou <zhou@0h7z.com>
2023-01-13 08:59:31 +0000

    Update - misc
    alerts.showFavicons                         => true
    browser.meta_refresh_when_inactive.disabled => true
    browser.newtabpage.activity-stream.weather.temperatureUnits => c
    browser.region.update.enabled               => false
    browser.send_pings.require_same_host        => true
    browser.translations.automaticallyPopup     => false
    devtools.debugger.remote-enabled            => false
    dom.ipc.plugins.reportCrashURL              => false
    dom.push.connection.enabled                 => false
    font.cjk_pref_fallback_order                => zh-cn,zh-tw,zh-hk,ja,ko
    full-screen-api.transition-duration.enter   => 100 0
    full-screen-api.transition-duration.leave   => 100 0
    full-screen-api.transition.timeout          => 200
    full-screen-api.warning.delay               => -1
    full-screen-api.warning.timeout             => -1
    general.config.obscure_value                => 0
    general.useragent.compatMode.firefox        => true
    media.default_volume                        => 0.5
    media.navigator.video.default_fps           => 60
    media.recorder.max_memory                   => 8388608
    media.videocontrols.picture-in-picture.video-toggle.has-used       => true
    media.videocontrols.picture-in-picture.video-toggle.min-video-secs => 10
    pointer-lock-api.warning.timeout            => -1
    privacy.donottrackheader.enabled            => true
    services.sync.telemetry.maxPayloadCount     => 0
    services.sync.telemetry.submissionInterval  => 86400
    telemetry.maxEventsCount                    => 0
    
    browser/components/newtab/lib/ActivityStream.sys.mjs
    dom/media/MediaRecorder.h
    gfx/thebes/PrintTargetPDF.cpp
    modules/libpref/init/all.js
    modules/libpref/init/StaticPrefList.yaml
    services/settings/Utils.sys.mjs
    services/sync/modules/telemetry.js
    toolkit/components/pdfjs/content/web/viewer.css
    toolkit/components/pdfjs/content/web/viewer.mjs
    toolkit/components/printing/content/print.js
    toolkit/components/utils/ClientEnvironment.jsm
    xpcom/base/AvailableMemoryWatcherWin.cpp

--- toolkit/components/pdfjs/content/web/viewer.mjs
+++ toolkit/components/pdfjs/content/web/viewer.mjs
@@ -3644,7 +3644,7 @@ class PDFCursorTools {
 ;// CONCATENATED MODULE: ./web/pdf_document_properties.js
 
 
-const NON_METRIC_LOCALES = ["en-us", "en-lr", "my"];
+const NON_METRIC_LOCALES = [];
 const US_PAGE_NAMES = {
   "8.5x11": "pdfjs-document-properties-page-size-name-letter",
   "8.5x14": "pdfjs-document-properties-page-size-name-legal"
@@ -3775,7 +3775,7 @@ class PDFDocumentProperties {
       };
     }
     const isPortrait = isPortraitOrientation(pageSizeInches),
-      nonMetric = NON_METRIC_LOCALES.includes(this.l10n.getLanguage());
+      nonMetric = false; // NON_METRIC_LOCALES.includes(this.l10n.getLanguage());
     let sizeInches = {
       width: Math.round(pageSizeInches.width * 100) / 100,
       height: Math.round(pageSizeInches.height * 100) / 100
@@ -3794,9 +3794,9 @@ class PDFDocumentProperties {
         width: Math.round(sizeMillimeters.width),
         height: Math.round(sizeMillimeters.height)
       };
-      if (Math.abs(exactMillimeters.width - intMillimeters.width) < 0.1 && Math.abs(exactMillimeters.height - intMillimeters.height) < 0.1) {
+      if (Math.abs(exactMillimeters.width - intMillimeters.width) < 1.0 && Math.abs(exactMillimeters.height - intMillimeters.height) < 1.0) {
         nameId = getPageName(intMillimeters, isPortrait, METRIC_PAGE_NAMES);
-        if (nameId) {
+        if (false) {
           sizeInches = {
             width: Math.round(intMillimeters.width / 25.4 * 100) / 100,
             height: Math.round(intMillimeters.height / 25.4 * 100) / 100
