227a72df5cb53e3f5a6acd9c3bcdcf1c5346d6d4
Heptazhou <zhou@0h7z.com>
2023-01-13 08:59:31 +0000

    Update
    alerts.showFavicons                         => true
    browser.meta_refresh_when_inactive.disabled => true
    browser.region.update.enabled               => false
    browser.send_pings.require_same_host        => true
    browser.translations.automaticallyPopup     => false
    devtools.debugger.remote-enabled            => false
    dom.ipc.plugins.reportCrashURL              => false
    dom.push.connection.enabled                 => false
    font.cjk_pref_fallback_order                => zh-cn,zh-tw,zh-hk,ja,ko
    full-screen-api.transition-duration.enter   => 100 0
    full-screen-api.transition-duration.leave   => 100 0
    full-screen-api.transition.timeout          => 200
    full-screen-api.warning.delay               => -1
    full-screen-api.warning.timeout             => -1
    general.config.obscure_value                => 0
    general.useragent.compatMode.firefox        => true
    image.http.accept                           => image/avif,image/webp,*/*
    media.default_volume                        => 0.5
    media.navigator.video.default_fps           => 60
    media.peerconnection.dtls.version.max       => 772
    media.recorder.max_memory                   => 8388608
    media.videocontrols.picture-in-picture.video-toggle.has-used       => true
    media.videocontrols.picture-in-picture.video-toggle.min-video-secs => 10
    pointer-lock-api.warning.timeout            => -1
    privacy.donottrackheader.enabled            => true
    privacy.fingerprintingProtection.overrides  => +AllTargets,-CSSPrefersColorScheme
    services.sync.telemetry.maxPayloadCount     => 0
    services.sync.telemetry.submissionInterval  => 86400
    telemetry.maxEventsCount                    => 0
    
    dom/media/MediaRecorder.h
    gfx/thebes/PrintTargetPDF.cpp
    modules/libpref/init/all.js
    services/sync/modules/telemetry.js
    toolkit/components/printing/content/print.js
    toolkit/components/utils/ClientEnvironment.jsm

--- services/sync/modules/telemetry.sys.mjs
+++ services/sync/modules/telemetry.sys.mjs
@@ -699,7 +699,7 @@ class SyncTelemetryImpl {
     this.migrations = [];
     this.maxEventsCount = lazy.Svc.PrefBranch.getIntPref(
       "telemetry.maxEventsCount",
-      1000
+      0
     );
     this.maxPayloadCount = lazy.Svc.PrefBranch.getIntPref(
       "telemetry.maxPayloadCount"
