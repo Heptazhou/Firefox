fa69857cf7362eeddbcfd7ff30f74c7e55f0103a
Heptazhou <zhou@0h7z.com>
2024-12-10 11:29:45 +0000

    Create
    browser/branding/snowfox/

---
+++ browser/branding/snowfox/pref/firefox-branding.js
@@ -0,0 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This file contains branding-specific prefs.
+
+// Number of usages of the web console.
+// If this is less than 5, then pasting code into the web console is disabled
+pref("devtools.selfxss.count", 5);
