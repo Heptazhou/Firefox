b59d74a277c295f8b06dccea526bc9bcbfb7e04a
Heptazhou <zhou@0h7z.com>
2025-04-30 04:52:09 +0000

    Update

--- workflows.jl
+++ workflows.jl
@@ -150,5 +150,5 @@ end
 # https://github.com/mozilla/gecko-dev/blob/master/.hgtags
 branch = sort!((f = "branch.toml") |> ODict ∘ TOML.parsefile)
 write(f, sprint(TOML.print, branch))
-make_vs(branch["FIREFOX_NIGHTLY_137_END"], :v137)
+make_vs(branch["FIREFOX_NIGHTLY_138_END"], :v138)
 
